922e392569171f2c35cb26f97422d0ab649fe430
cstewart86 <52621007+cstewart86@users.noreply.github.com>
2019-07-15 11:18:44 +0100

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 #Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
 require "uglifier"
+require "bundler" Bundler.setup(: default)
 
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
